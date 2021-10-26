98b6cecdd95204ebe915959696f38cf0c69c7b53
Narasimha <nraovuppala@gmail.com>
2021-10-26 18:16:33 +0530

    first commit

---
+++ src/test/java/com/demo/spring/springconfigserver/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.spring.springconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
